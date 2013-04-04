33d11c903c64afa51aed1efd2db092aed53474b5
johannespostler <johannes.postler@uibk.ac.at>
2013-04-04 14:14:38 +0200

    Add feature to loglib: Logging to variables (no file, no stdout)

--- loglib.py
+++ loglib.py
@@ -8,48 +8,58 @@ import datetime
 
 class Log:
     """This class provides logging functions"""
-    def __init__(self, filename = None, outputfolder = 'output'):
+    def __init__(self, filename = None, outputfolder = 'output', tovariable = False):
     
         #make a timestamp
         self.starttime = datetime.datetime.now()
         
-        self.outputfolder = outputfolder
+        #shall we log everything to a variable?
+        if tovariable = True:
+            #we obviously want to write to a variable
+            self.logcontent = ''
+            self.nologfile = True
+            self.tovariable = True
+
+        elif tovariable = False:
+            self.outputfolder = outputfolder
+            
+            #create a default
+            if filename is None:
+                filename = (self.outputfolder + '/output_%s.log' % self.starttime.strftime('%d_%m_%Y_%Hh%Mm%S'))
         
-        #create a default
-        if filename is None:
-            filename = (self.outputfolder + '/output_%s.log' % self.starttime.strftime('%d_%m_%Y_%Hh%Mm%S'))
-        
-        #let's assume we can create a logfile
-        self.nologfile = False
+            #let's assume we can create a logfile
+            self.nologfile = False
 
 	    #adjust file path in case we're running on fucking windows
-        self.filename = os.path.normcase(filename)
+            self.filename = os.path.normcase(filename)
                 
-        #make an absolute path for fucking windows
-        self.filename = os.path.join(os.path.dirname(sys.argv[0]), self.filename)
+            #make an absolute path for fucking windows
+            self.filename = os.path.join(os.path.dirname(sys.argv[0]), self.filename)
         
-        #see if the given or standard logfile can be opened
-        try:
-            self.fh = open(self.filename, 'w')
-        except IOError:
-            #maybe it is supposed to be in a subdirectory and we have to the create said directory
-            directory = os.path.dirname(self.filename)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-
+            #see if the given or standard logfile can be opened
             try:
                 self.fh = open(self.filename, 'w')
-            except:
-                self.nologfile = True
+            except IOError:
+                #maybe it is supposed to be in a subdirectory and we have to the create said directory
+                directory = os.path.dirname(self.filename)
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
 
-        if self.nologfile is not True:
-            self.write('New logfile created. It is now: %s' % self.starttime)
-        else:
-            self.write('Warning! Could not create logfile! Giving all output to stdout!')
+                try:
+                    self.fh = open(self.filename, 'w')
+                except:
+                    self.nologfile = True
+
+            if self.nologfile is not True:
+                self.write('New logfile created. It is now: %s' % self.starttime)
+            else:
+                self.write('Warning! Could not create logfile! Giving all output to stdout!')
 
     def write(self, text):
         if self.nologfile is True:
             print text + '\r\n'
+        elif self.nologfile is  True and self.tovariable is True:
+            self.logcontent = self.logcontent + text + '\r\n'
         else:
             self.fh.write(text + '\r\n')
 
