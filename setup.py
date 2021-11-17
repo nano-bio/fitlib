6719bc65337ed46162bc22cbc498c074b9478d8a
Graf-Zahl <Graf-Zahl@users.noreply.github.com>
2021-11-17 14:27:57 +0100

    replaced sudo-jerk by the community edition

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'suds-jurko',
+        'suds-community',
     ],
     python_requires='>=3.6',
 )
